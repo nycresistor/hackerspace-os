b50ba71d4daaedf919ba5d31431068a1e6804ab8
fin <fin@xbhd.org>
2008-12-17 13:37:47 +0000

    added migrations directory & first migration for the latest model change
    git-svn-id: http://hackerspace-os.googlecode.com/svn/trunk@71 a34140dc-3d51-0410-a734-3fb14a677ee6

---
+++ mos/migrations/001.sql
@@ -0,0 +1,3 @@
+ALTER TABLE members_payment ADD `original_line` longtext NULL;
+ALTER TABLE members_payment ADD `original_file` varchar(200) NULL;
+ALTER TABLE members_payment ADD `original_lineno` integer NULL;
