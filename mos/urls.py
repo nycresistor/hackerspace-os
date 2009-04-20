43d2adf6a81036fe0adeb071f97fa432e48aaf3b
fin <fin@xbhd.org>
2009-04-20 22:06:49 +0000

    re-activate rssfeeds
    git-svn-id: http://hackerspace-os.googlecode.com/svn/trunk@75 a34140dc-3d51-0410-a734-3fb14a677ee6

--- mos/urls.py
+++ mos/urls.py
@@ -14,8 +14,8 @@ feeds = {
 admin.autodiscover()
 
 urlpatterns = patterns('',
- #   (r'^feeds/(?P<url>.*)/$',
-#      'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+   (r'^feeds/(?P<url>.*)/$',
+      'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
 
     (r'^calendar/', include('mos.cal.urls')),
     (r'^rss/', include('mos.rss.urls')),
